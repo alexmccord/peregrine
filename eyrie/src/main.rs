e5eed20e23bddef847c03708372cd64299d3087d
Alexander McCord <alex@mccord.dev>
2025-01-05 21:02:32 -0800

    Rename prgc to eyrie. And start a build driver.

---
+++ eyrie/src/main.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+
+use clap;
+
+use peregrine::pipeline::driver::{BuildDriver, BuildTask};
+
+#[derive(clap::Parser, Debug)]
+struct Build {
+    pub dir: Option<PathBuf>,
+}
+
+#[derive(clap::Parser, Debug)]
+enum Command {
+    Build(Build),
+}
+
+fn main() {
+    let args = clap::Parser::parse();
+
+    match args {
+        Command::Build(b) => build(b),
+    }
+}
+
+fn build(build: Build) {
+    let prg = build.dir.unwrap();
+
+    let mut driver = BuildDriver::new();
+    driver.submit_task(BuildTask::Project(prg));
+}
--- peregrine/src/pipeline/driver.rs
+++ peregrine/src/pipeline/driver.rs
@@ -1,16 +1,35 @@
+use std::collections::VecDeque;
 use std::path::PathBuf;
 
-use crate::syntax::ast::AstAllocator;
+use crate::syntax::ast::Program;
 use crate::syntax::parser::Parser;
 
-struct BuildDriver {
-    pending: Vec<PathBuf>,
+pub enum BuildTask {
+    Project(PathBuf),
+}
+
+pub struct BuildDriver {
+    pending: VecDeque<BuildTask>,
 }
 
 impl BuildDriver {
     pub fn new() -> BuildDriver {
         BuildDriver {
-            pending: Vec::default(),
+            pending: VecDeque::default(),
         }
     }
+
+    pub fn submit_task(&mut self, task: BuildTask) {
+        self.pending.push_back(task);
+    }
+
+    pub fn execute(&mut self) {
+        while let Some(task) = self.pending.pop_front() {
+            match task {
+                BuildTask::Project(path) => self.build_project(path),
+            }
+        }
+    }
+
+    fn build_project(&mut self, path: PathBuf) {}
 }
